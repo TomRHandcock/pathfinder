f126213aa43f5834d8ec8ae93d24d934d0680774
Tom Handcock <33254448+TomRHandcock@users.noreply.github.com>
2023-09-02 19:53:16 +0100

    feat: Implement 'EditorCubit'

---
+++ lib/ui/screens/editor/cubit/editor_cubit.dart
@@ -0,0 +1,22 @@
+import 'package:pathfinder/engine/model/graph/edge.dart';
+import 'package:pathfinder/engine/model/graph/graph.dart';
+import 'package:pathfinder/engine/model/graph/node.dart';
+import 'package:pathfinder/ui/screens/editor/cubit/editor_state.dart';
+import 'package:bloc/bloc.dart';
+
+class EditorCubit extends Cubit<EditorState> {
+  EditorCubit()
+      : super(
+          const EditorState(
+            graph: Graph(
+              nodes: [
+                Node(id: 0, xPosition: 100, yPosition: 100, label: "Node 1"),
+                Node(id: 1, xPosition: 100, yPosition: 200, label: "Node 2"),
+              ],
+              edges: [
+                Edge(startId: 0, endId: 1),
+              ],
+            ),
+          ),
+        );
+}
