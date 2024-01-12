ecf13550f78249398587e1cbf70e14ca2fc68c07
Tom Handcock <33254448+TomRHandcock@users.noreply.github.com>
2024-01-12 23:32:49 +0000

    chore: Register `EditorCubit` with GetIt

--- lib/engine/service_locator/cubit_setup.dart
+++ lib/engine/service_locator/cubit_setup.dart
@@ -1,6 +1,7 @@
 part of 'main_setup.dart';
 
 _setupCubits() {
+  _getIt.registerFactory(() => EditorCubit());
   _getIt.registerFactory(() => CanvasCubit());
   _getIt.registerFactory(() => NewNodeTemplateCubit());
 }
