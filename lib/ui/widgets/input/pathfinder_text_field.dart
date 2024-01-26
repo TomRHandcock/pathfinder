085661f4181f93424bd98456ef864638d3f14866
Tom Handcock <33254448+TomRHandcock@users.noreply.github.com>
2024-01-26 23:46:51 +0000

    feat: Add `onChanged` param to `PathfinderTextField`

--- lib/ui/widgets/input/pathfinder_text_field.dart
+++ lib/ui/widgets/input/pathfinder_text_field.dart
@@ -3,9 +3,11 @@ import 'package:pathfinder/ui/util/context_utils.dart';
 
 class PathfinderTextField extends StatelessWidget {
   final TextEditingController controller;
+  final Function(String value)? onChanged;
 
   const PathfinderTextField({
     required this.controller,
+    this.onChanged,
     super.key,
   });
 
@@ -15,6 +17,7 @@ class PathfinderTextField extends StatelessWidget {
       controller: controller,
       cursorColor: context.pathfinderTheme.colors.textColor,
       decoration: context.pathfinderTheme.input.textfield,
+      onChanged: onChanged,
       style: context.pathfinderTheme.text.bodyLarge,
     );
   }
